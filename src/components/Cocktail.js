df39069919071d4f9ee5f3eb2b8c4a44d3e7bf5f
RitwikNandi <ritwiknandi006@gmail.com>
2023-02-14 02:51:09 +0530

    first commit

---
+++ src/components/Cocktail.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Cocktail = ({ image, name, info, id, glass }) => {
+  return (
+    <article className='cocktail' key={id}>
+      <Link to={`/cocktail/${id}`} className='btn btn-primary btn-details'>
+        <div className='img-container'>
+          <img src={image} alt={name} />
+        </div>
+        <div className='cocktail-footer'>
+          <h3>{name}</h3>
+          <h4>{glass}</h4>
+          <p>{info}</p>
+        </div>
+      </Link>
+    </article>
+  );
+};
+
+export default Cocktail;
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi,
+        assumenda debitis. Nihil vitae a nemo iure tenetur soluta itaque magnam
+        repellendus ad! Necessitatibus, dolorem consectetur est quo non
+        perspiciatis minima!
+      </p>
+    </section>
+  );
+};
+
+export default About;
